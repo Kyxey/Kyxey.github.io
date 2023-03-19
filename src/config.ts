7cbd98b27ec6dc3e945b3963f8c7deeaa0961f63
Kyxey <maniazizjahan@gmail.com>
2023-03-19 02:46:51 +0100

    feat: adds the new portfolio with Astro

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Ali Azizjahan (Kyxey) Portfolio";
+export const SITE_DESCRIPTION =
+  "Get to know Ali Azizjahan (Kyxey) better or download his resume.";
